cf5a075c85e9c0535e9ddfc42c8e0e8eb532944c
Eulga <kimyeoun10@gmail.com>
2023-09-04 00:16:25 +0900

    feat: 웹 소켓 분리

--- src/main/java/com/example/pt_websocket/domain/websocket/event/EventScheduler.java
+++ src/main/java/com/example/pt_websocket/domain/websocket/event/EventScheduler.java
@@ -4,7 +4,6 @@ import com.example.pt_websocket.domain.websocket.service.QueueService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,14 +12,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class EventScheduler {
     private final QueueService queueService;
-    private final SimpMessageSendingOperations simpMessageSendingOperations;
 
     @Scheduled(fixedDelay = 1000)
     public void getOrder() throws JsonProcessingException {
-        log.info("do work");
-        simpMessageSendingOperations.convertAndSend("/sub/hey", "sup!");
-
-//        queueService.getOrder(Event.REGISTRATION);
+        queueService.getOrder(Event.REGISTRATION);
     }
 
     @Scheduled(fixedDelay = 100)
