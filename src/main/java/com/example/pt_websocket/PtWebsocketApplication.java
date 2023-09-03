84f56c89212b587acb543dd2658d1996a82b5b17
Eulga <kimyeoun10@gmail.com>
2023-09-04 00:15:58 +0900

    feat: 웹 소켓 분리

--- src/main/java/com/example/pt_websocket/PtWebsocketApplication.java
+++ src/main/java/com/example/pt_websocket/PtWebsocketApplication.java
@@ -2,7 +2,9 @@ package com.example.pt_websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PtWebsocketApplication {
 
